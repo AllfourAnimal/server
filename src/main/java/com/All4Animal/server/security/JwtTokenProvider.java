79bacae04c1c3859f16b496e52ac714c9c6e4f01
KimJungMook <wjdanr0869@naver.com>
2026-04-08 18:50:11 +0900

    fix : jwt관련 수정

--- src/main/java/com/All4Animal/server/security/JwtTokenProvider.java
+++ src/main/java/com/All4Animal/server/security/JwtTokenProvider.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
@@ -20,7 +21,7 @@ public class JwtTokenProvider {
             @Value("${jwt.secret}") String secret,
             @Value("${jwt.access-token-expiration}") long accessTokenExpiration
     ) {
-        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
         this.accessTokenExpiration = accessTokenExpiration;
     }
 
