c30e3df5495a24555b398e229370cc43340d9f97
KimJungMook <wjdanr0869@naver.com>
2026-04-08 14:51:14 +0900

    fix: FavoriteController 생성 및 ReviewController 예제 생성

--- src/main/java/com/All4Animal/server/controller/FavoriteController.java
+++ src/main/java/com/All4Animal/server/controller/FavoriteController.java
@@ -1,14 +1,21 @@
 package com.All4Animal.server.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/review")
-public class ReviewController {
+@RequestMapping("/api/favorite")
+@Tag(name = "Favorite", description = " API")
+@SecurityRequirements
+@RequiredArgsConstructor
+public class FavoriteController {
 
     @GetMapping("")
     public ResponseEntity<?> getExample(){
