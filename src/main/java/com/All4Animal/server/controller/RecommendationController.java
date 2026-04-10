a836293b2ce702e484b05140a21c1fa91e9c6cb6
KimJungMook <wjdanr0869@naver.com>
2026-04-10 14:45:41 +0900

    fix : 사용하지 않는 import문 삭제

--- src/main/java/com/All4Animal/server/controller/RecommendationController.java
+++ src/main/java/com/All4Animal/server/controller/RecommendationController.java
@@ -1,13 +1,10 @@
 package com.All4Animal.server.controller;
 
 
-import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
