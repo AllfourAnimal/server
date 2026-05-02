f0586c9368ff4f64b83039bea48227965404fd45
KimJungMook <wjdanr0869@naver.com>
2026-05-02 18:31:37 +0900

    fix : cors문제 해결

--- src/main/java/com/All4Animal/server/config/SwaggerConfig.java
+++ src/main/java/com/All4Animal/server/config/SwaggerConfig.java
@@ -7,7 +7,12 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @OpenAPIDefinition(
@@ -26,4 +31,11 @@ import org.springframework.context.annotation.Configuration;
         in = SecuritySchemeIn.HEADER
 )
 public class SwaggerConfig {
+        @Bean
+        public OpenAPI openAPI() {
+                return new OpenAPI()
+                        .servers(List.of(
+                                new Server().url("https://all4animal.site")
+                        ));
+        }
 }
