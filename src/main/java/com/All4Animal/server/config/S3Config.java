18dccb9a9979a493b6850c3dd9abab7ba71b7ce7
KimJungMook <wjdanr0869@naver.com>
2026-05-04 22:34:27 +0900

    create : S3 관련 내용 추가

---
+++ src/main/java/com/All4Animal/server/config/S3Config.java
@@ -0,0 +1,31 @@
+package com.All4Animal.server.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        /* 두개의 키로 자격증명 객체 생성 */
+        AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials)) // 자격 증명을 제공하는 provider
+                .build();
+    }
+}
